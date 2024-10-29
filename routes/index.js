32bd6249df6695e8156b4c286960e006849a4e3c
S-Skaggs <s-skaggs@outlook.com>
2024-10-29 09:35:11 -0400

    FEAT: Added route for api/composers

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,25 @@ var express = require('express');
 var router = express.Router();
 const { composerService } = require('../model');
 
+/*
+  Can test this route from a second terminal using
+  node -e "http.get('http://localhost:3000/', (res) => res.setEncoding('utf8').once('data', console.log))"
+*/
 router.get('/', async function(req, res, next) {
+  try{
+    const composers = await composerService.getAllComposers();
+    res.json(composers);
+  } catch (err) {
+    console.error(err);
+    next(err);
+  }
+});
+
+/*
+  Can test this route from a second terminal using
+  node -e "http.get('http://localhost:3000/api/composers', (res) => res.setEncoding('utf8').once('data', console.log))"
+*/
+router.get('/api/composers', async function(req, res, next) {
   try{
     const composers = await composerService.getAllComposers();
     res.json(composers);
